6011da73b764af646bcb55a27906d665c54d3504
Jennifer Park <zp@Jennifers-MacBook-Air.local>
2021-03-13 16:40:32 -0800

    Completed multiply function

--- arithmetic.py
+++ arithmetic.py
@@ -12,7 +12,7 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-    
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
