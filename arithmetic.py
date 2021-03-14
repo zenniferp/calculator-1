7646e38fd85ae0cf5221229c0a51ca8cd96c43be
Jennifer Park <zp@Jennifers-MacBook-Air.local>
2021-03-13 16:45:56 -0800

    Completed divide function

--- arithmetic.py
+++ arithmetic.py
@@ -16,7 +16,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
