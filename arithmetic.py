72b85f55f6fbad366e1eca7203880ef261676164
Jennifer Park <zp@Jennifers-MacBook-Air.local>
2021-03-13 17:23:31 -0800

    Completed addmult fn

--- arithmetic.py
+++ arithmetic.py
@@ -33,3 +33,7 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Return sum of first two multiplied by third"""
+    return (num1 + num2)*num3
